579b437933489f2e807e76c3872a78c876d1efa7
rune004 <christian.rune1998@gmail.com>
2023-09-17 21:55:56 +0200

    Hotfix to packages in overview

--- CnC-Agent/Debian-Scripts/Overview.sh
+++ CnC-Agent/Debian-Scripts/Overview.sh
@@ -6,7 +6,7 @@ ip=$(hostname -I | awk '{print $1}')
 
 mac_address=$(cat /sys/class/net/*/address | sed -n '1 p')
 
-packages=$(apt-get -s --no-download dist-upgrade -V --fix-missing | grep '=>')
+packages=$(apt-get -q -y --ignore-hold --allow-change-held-packages --allow-unauthenticated -s dist-upgrade | /bin/grep  ^Inst | wc -l)
 
 if [ -f /etc/os-release ]; then
     # freedesktop.org and systemd
