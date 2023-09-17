bed88a283f6c02f38e558d3dcdd8d993637355f0
rune004 <christian.rune1998@gmail.com>
2023-09-17 23:10:54 +0200

    Update to Cronjob

--- CnC-Agent/Debian-Scripts/Cronjob-test.sh
+++ CnC-Agent/Debian-Scripts/Cronjob-test.sh
@@ -1,27 +1,37 @@
+#!/bin/bash
 databaseip=$(cat ~/CnC-WebGUI/CnC-Agent/.databaseip)
 hn=$(echo $HOSTNAME)
 
-olddata=$(mysql --host=$databaseip --port=3306 --user=root --password=12Marvel machines << EOF
-SELECT * FROM cronjobs WHERE hostname LIKE "$hn"
-EOF)
+job1=$(crontab -l | sed -n '1 p')
 
-for data in "${olddata[@]}" ; do
-    if [ $olddata == "$data" ]; then
-    
-    break;
-    
-    else 
-    
-        for insert in "${olddata[@]}" ; do
-        
-        job1=$(crontab -l | sed -n '1 p')
-        
-        mysql --host=$databaseip --port=3306 --user=root --password=12Marvel machines << EOF
-        insert into cronjobs (hostname,cron_jobs_scripts) values('$hn','$job1');
-        EOF
-    
+# MySQL database connection details
+DB_HOST="$databaseip"
+DB_PORT="3306"
+DB_USER="root"
+DB_PASSWORD="12Marvel"
+DB_NAME="machines"
+START=1
+END=20
+## save $START, just in case if we need it later ##
+i=$START
+while [[ $i -le $END ]]
+do
+    job1=$(crontab -l | sed -n '1 p')
 
-    done
+    # Data to insert if it doesn't exist in the database
+    VAR1="$hn"
+    VAR2="$job1"
 
+    # Check if the data already exists in the database
+    existing_data=$(mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p"$DB_PASS>
+
+    # If no rows were returned, insert the data
+    if [ -z "$existing_data" ]; then
+    echo "Inserting data into the database..."
+    mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p"$DB_PASSWORD" -D "$DB_NA>
+    echo "Data inserted successfully."
+    else
+    echo "Data already exists in the database."
+    fi
+   ((i = i + 1))
 done
-
