cf601a573b52dcdc37d7145fb790275afb00f82a
rune004 <christian.rune1998@gmail.com>
2023-09-17 23:13:47 +0200

    Fixed missing code

--- CnC-Agent/Debian-Scripts/Cronjob-test.sh
+++ CnC-Agent/Debian-Scripts/Cronjob-test.sh
@@ -11,7 +11,7 @@ DB_USER="root"
 DB_PASSWORD="12Marvel"
 DB_NAME="machines"
 START=1
-END=20
+END=5
 ## save $START, just in case if we need it later ##
 i=$START
 while [[ $i -le $END ]]
@@ -23,12 +23,12 @@ do
     VAR2="$job1"
 
     # Check if the data already exists in the database
-    existing_data=$(mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p"$DB_PASS>
+    existing_data=$(mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p"$DB_PASSWORD" -D "$DB_NAME" -e "SELECT * FROM cronjobs WHERE hostname='$VAR1' AND cron_jobs_scripts='$VAR2';")
 
     # If no rows were returned, insert the data
     if [ -z "$existing_data" ]; then
     echo "Inserting data into the database..."
-    mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p"$DB_PASSWORD" -D "$DB_NA>
+    mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p"$DB_PASSWORD" -D "$DB_NAME" -e "INSERT INTO cronjobs (hostname, cron_jobs_scripts) VALUES ('$VAR1', '$VAR2');"
     echo "Data inserted successfully."
     else
     echo "Data already exists in the database."
