f7624c15b53554aee6bcc1e9255f264eaf7135f2
rune004 <christian.rune1998@gmail.com>
2023-09-17 23:44:58 +0200

    Minor test

--- CnC-Agent/Debian-Scripts/Cronjob-test.sh
+++ CnC-Agent/Debian-Scripts/Cronjob-test.sh
@@ -24,7 +24,7 @@ do
     job3=$(crontab -l | grep -i Cronjob-test | sed -n '1 p')
 
     else
-        echo "Failed to run"
+        echo ""
     fi
     # Data to insert if it doesn't exist in the database
     VAR1="$hn"
@@ -35,7 +35,7 @@ do
     elif [ $i == 3 ]; then
     VAR2="$job3"
     else
-        echo "Failed to run"
+        echo ""
     fi
 
     # Check if the data already exists in the database
