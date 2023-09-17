9a1e073f3864a4309db924fcb3a669cd2dc21363
rune004 <christian.rune1998@gmail.com>
2023-09-17 12:09:17 +0200

    Hot fix

--- CnC-Agent/Installers/Debian-Installer.sh
+++ CnC-Agent/Installers/Debian-Installer.sh
@@ -2,12 +2,13 @@ read -p "Database IP: " databaseip
 
 ##
 touch ~/CnC-WebGUI/CnC-Agent/.databaseip
-echo "$databaseip" > ~/CnC-Agent/.databaseip
+echo "$databaseip" > ~/CnC-WebGUI/CnC-Agent/.databaseip
 
 ## Check/Setup Packages Reporting via cron 
 ln -s ~/CnC-WebGUI/CnC-Agent/Debian-Scripts/Packages-test.sh /usr/bin/ > /dev/null 2>&1
 
-crontab -l > file; echo '00 00 * * * ruby ~/CnC-WebGUI/CnC-Agent/Debian-Scripts/Packages-test.sh >/dev/null 2>&1' >> file; crontab file
+crontab -l > file; echo '00 00 * * * ruby ~CnC-WebGUI/CnC-Agent/Debian-Scripts/Packages-test.sh >/dev/null 2>&1' >> file; crontab file
 
 ## Run Packages Reporting for the first time
-bash ~/CnC-Agent/Debian-Scripts/Packages-test.sh
+bash ~/CnC-WebGUI/CnC-Agent/Debian-Scripts/Packages-test.sh
+
