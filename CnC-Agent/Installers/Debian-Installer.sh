2c1acf91022b80a14310604e7cc6ecdf2e493d49
rune004 <christian.rune1998@gmail.com>
2023-09-18 22:29:55 +0200

    Minor fix and clean up in debian agent installer

--- CnC-Agent/Installers/Debian-Installer.sh
+++ CnC-Agent/Installers/Debian-Installer.sh
@@ -1,26 +1,34 @@
-echo "Installing MySQL"
+## Update and install gnupg
+clear
+echo "Installing CnC-Agent"
 apt update > /dev/null 2>&1
 apt install gnupg -y > /dev/null 2>&1
 
+
+## Download MySQL
+clear
+echo "Installing MySQL"
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.22-1_all.deb -P /tmp > /dev/null 2>&1
-
-## dpkg -i /tmp/mysql-apt-config* 
-
 apt update > /dev/null 2>&1
+
+## Install and set up MySQL
+clear 
+echo "Setting Up MySQL"
 
 debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password 12Marvel"
 debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password 12Marvel"
 
 DEBIAN_FRONTEND=noninteractive apt-get -y install mysql-server
 
-clear 
-
 systemctl stop mysql > /dev/null 2>&1
 systemctl disable mysql > /dev/null 2>&1
 
+
+## Get database IP address
+clear 
 read -p "Database IP: " databaseip
 
-##
+## Save database IP address
 touch ~/CnC-WebGUI/CnC-Agent/.databaseip
 echo "$databaseip" > ~/CnC-WebGUI/CnC-Agent/.databaseip
 
