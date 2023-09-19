78e3ee3f0c719fce3b6528ca2df8248ef39f2234
rune004 <christian.rune1998@gmail.com>
2023-09-19 08:08:05 +0200

    Testing

--- CnC-Agent/Debian-Scripts/Packages.sh
+++ CnC-Agent/Debian-Scripts/Packages.sh
@@ -46,7 +46,8 @@ if [ -n "$existing_data" ]; then
         WHERE hostname='$HOSTNAME'" 
 else
     # Insert a new record
-    mysql -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_NAME -e 2>/dev/null "INSERT INTO packages (
+    mysql -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_NAME -e 2>/dev/null <<EOF 
+    INSERT INTO packages (
         hostname,
         git,
         wget,
@@ -74,5 +75,6 @@ else
         '$DOCKER_COMPOSE_PLUGIN',
         '$CURL',
         '$CONTAINERD'
-    )"
+    );
+EOF
 fi
