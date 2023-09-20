63edb9277456464f49abb768ca5c8ebe2404ca9f
rune004 <christian.rune1998@gmail.com>
2023-09-20 12:58:38 +0200

    Minor fix to Download Agent

--- Download-Agent.sh
+++ Download-Agent.sh
@@ -1,6 +1,6 @@
 rm -r CnC-WebGUI/
 
-git clone https://github.com/rune004/CnC-WebGUI.git
+git clone --branch Production https://github.com/rune004/CnC-WebGUI.git
 
 bash ~/CnC-WebGUI/CnC-Agent/Install-Agent.sh
 
