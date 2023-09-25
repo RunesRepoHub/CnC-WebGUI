3a40778fc4400a5c395320ea677ee3f411a458df
Rune <christian.rune1998@gmail.com>
2023-09-25 15:03:49 +0200

    Update to self hosted repo

--- Download-Agent.sh
+++ Download-Agent.sh
@@ -1,6 +1,6 @@
 rm -r CnC-WebGUI/
 
-git clone --branch Production https://github.com/rune004/CnC-WebGUI.git
+git clone --branch Production https://git.rp-helpdesk.com/Rune/CnC-WebGUI.git
 
 bash ~/CnC-WebGUI/CnC-Agent/Install-Agent.sh
 
