2d171ed46ded3474c1e71298d0df9467e4159352
rune <christian.rune1998@gmail.com>
2023-09-26 15:30:34 +0200

    Testing Version Control

--- CnC-Image-Builder.sh
+++ CnC-Image-Builder.sh
@@ -1,10 +1,29 @@
-version=$(cat ~/CnC-WebGUI/Version.txt)
+GIT_Version=$("`wget -qO- https://git.rp-helpdesk.com/Rune/CnC-WebGUI/raw/branch/Dev/Version.txt`")
+LOCAL_Version=$(cat ~/CnC-WebGUI/Version.txt)
 
-touch ~/CnC-WebGUI/CnC-WebGUI/.env
-echo "version=$version" > ~/CnC-WebGUI/CnC-WebGUI/.env
+if [ $GIT_Version >= $LOCAL_Version ]; then
+
+    version=$(cat ~/CnC-WebGUI/Version.txt)
+
+    touch ~/CnC-WebGUI/CnC-WebGUI/.env
+    echo "version=$version" > ~/CnC-WebGUI/CnC-WebGUI/.env
 
 
-docker build -t cnc-web:$version ~/CnC-WebGUI/CnC-WebGUI/Nginx-Docker
-docker build -t cnc-mysql:$version ~/CnC-WebGUI/CnC-WebGUI/MySQL-Docker
+    docker build -t cnc-web:$version ~/CnC-WebGUI/CnC-WebGUI/Nginx-Docker
+    docker build -t cnc-mysql:$version ~/CnC-WebGUI/CnC-WebGUI/MySQL-Docker
 
-docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui up -d
+    docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui up -d
+else 
+
+    echo "There is a newer version of CnC-WebGUI"
+    echo "Do you want to update first?"
+
+    read -p "Are you sure? " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        # do dangerous stuff
+        bash ~/CnC-WebGUI/Download-Agent.sh
+
+    fi 
+fi
--- Download-Agent.sh
+++ Download-Agent.sh
@@ -1,13 +1,13 @@
 GIT_Version=$("`wget -qO- https://git.rp-helpdesk.com/Rune/CnC-WebGUI/raw/branch/Dev/Version.txt`")
 LOCAL_Version=$(cat ~/CnC-WebGUI/Version.txt)
 
-if [ $GIT_Version == $LOCAL_Version ]; then 
+if [ $GIT_Version >= $LOCAL_Version ]; then 
     
     bash ~/CnC-WebGUI/CnC-Agent/Install-Agent.sh
 
 else 
 
-    rm -r CnC-WebGUI/
+    rm -r ~/CnC-WebGUI
 
     git clone --branch Production https://git.rp-helpdesk.com/Rune/CnC-WebGUI.git
 
