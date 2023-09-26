ed3d4c886959825fc20cec31a1ee1dfe15dc1ce1
rune <christian.rune1998@gmail.com>
2023-09-26 17:49:27 +0200

    Minor Fix

--- Functions/Getting-started.sh
+++ Functions/Getting-started.sh
@@ -2,7 +2,7 @@
 
 PS3="Select the what you want to install on this server:"
 
-items=("Agent" "Server" "Quit")
+items=("Server" "Agent")
 
 while true; do
     select item in "${items[@]}" Quit
@@ -10,7 +10,6 @@ while true; do
         case $REPLY in
             1) bash ~/CnC-WebGUI/CnC-Agent/Install-Agent.sh; break;;
             2) bash ~/CnC-WebGUI/Functions/CnC-Image-Builder.sh; break;;
-            3) break;;
             $((${#items[@]}+1))) echo "We're done!"; break 2;;
             *) echo "Ooops - unknown choice $REPLY"; break;
         esac
