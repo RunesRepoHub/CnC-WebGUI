a2fe7d14ce19ccf80cee00af473a48783fcca6db
rune <christian.rune1998@gmail.com>
2023-09-26 16:30:42 +0200

    Suppress Output

--- Update.sh
+++ Update.sh
@@ -3,13 +3,15 @@ LOCAL_Version=$(cat ~/CnC-WebGUI/Version.txt)
 
 if [ $GIT_Version > "$LOCAL_Version" ]; then 
 
-    rm -r ~/CnC-WebGUI
+    rm -r ~/CnC-WebGUI >> ~/CnC-WebGUI/Logs/Update.log
 
-    cd ~/
+    cd ~/ >> ~/CnC-WebGUI/Logs/Update.log
 
-    git clone --branch Production https://git.rp-helpdesk.com/Rune/CnC-WebGUI.git
+    git clone --branch Production https://git.rp-helpdesk.com/Rune/CnC-WebGUI.git >> ~/CnC-WebGUI/Logs/Update.log
 
-    rm ~/Update.sh
+    rm ~/Update.sh >> ~/CnC-WebGUI/Logs/Update.log
+
+    echo "The latest Version has been download and installed"
 else  
     echo "An Error was incountered"
     echo "Error code 11 (Version Control Failed)"
