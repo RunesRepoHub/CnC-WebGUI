ae836eb2a58b596394511f0ae58d2a21e07eac16
rune <christian.rune1998@gmail.com>
2023-09-26 17:58:20 +0200

    Added a log for the image builder

--- Functions/CnC-Image-Builder.sh
+++ Functions/CnC-Image-Builder.sh
@@ -3,7 +3,9 @@ version=$(cat ~/CnC-WebGUI/Functions/Version.txt)
 touch ~/CnC-WebGUI/CnC-WebGUI/.env
 echo "version=$version" > ~/CnC-WebGUI/CnC-WebGUI/.env
 
-docker build -t cnc-web:$version ~/CnC-WebGUI/CnC-WebGUI/Nginx-Docker
-docker build -t cnc-mysql:$version ~/CnC-WebGUI/CnC-WebGUI/MySQL-Docker
+docker build -t cnc-web:$version ~/CnC-WebGUI/CnC-WebGUI/Nginx-Docker >> ~/CnC-WebGUI/Logs/CnC-Image-Builder.log
+docker build -t cnc-mysql:$version ~/CnC-WebGUI/CnC-WebGUI/MySQL-Docker >> ~/CnC-WebGUI/Logs/CnC-Image-Builder.log
+
+clear
 
 docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui up -d
