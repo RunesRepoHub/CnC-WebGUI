5d1cb1af00d6260603b6513c7baabb31c4d4bf50
rune <christian.rune1998@gmail.com>
2023-09-26 06:43:47 +0200

    Hotfix

--- Download-Agent.sh
+++ Download-Agent.sh
@@ -1,8 +1,7 @@
 rm -r CnC-WebGUI/
-USERNAME=$(whoami)
 folder="CnC-WebGUI/CnC-Agent"
 
 git clone --branch Production https://git.rp-helpdesk.com/Rune/CnC-WebGUI.git
 
-bash ~/$USERNAME/$folder/Install-Agent.sh
+bash ~/$folder/Install-Agent.sh
 
