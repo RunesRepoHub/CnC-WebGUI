fc5fec40b5c721b2518c7cceec82c69ef51a62ba
rune <christian.rune1998@gmail.com>
2023-09-26 06:04:08 +0200

    Hotfix

--- CnC-Image-Builder.sh
+++ CnC-Image-Builder.sh
@@ -1,4 +1,5 @@
-bash /$USERNAME/CnC-WebGUI/.metafile.sh
+
+bash /$USERNAME/CnC-WebGUI/metafile.sh
 
 docker build -t cnc-web:$version /$USERNAME/CnC-WebGUI/CnC-WebGUI/Nginx-Docker
 docker build -t cnc-mysql:$version /$USERNAME/CnC-WebGUI/CnC-WebGUI/MySQL-Docker
