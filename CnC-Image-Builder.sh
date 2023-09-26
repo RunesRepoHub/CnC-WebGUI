ce056f4f4d9ef2c7b336eacebeeeec77dfa8746c
rune <christian.rune1998@gmail.com>
2023-09-26 06:17:33 +0200

    Testing

--- CnC-Image-Builder.sh
+++ CnC-Image-Builder.sh
@@ -1,6 +1,10 @@
 USERNAME=$(whoami)
 version=$(cat /$USERNAME/CnC-WebGUI/Version.txt)
 
+touch /$USERNAME/CnC-WebGUI/CnC-WebGUI/.env
+echo "version=$version" > /$USERNAME/CnC-WebGUI/CnC-WebGUI/.env
+
+
 docker build -t cnc-web:$version /$USERNAME/CnC-WebGUI/CnC-WebGUI/Nginx-Docker
 docker build -t cnc-mysql:$version /$USERNAME/CnC-WebGUI/CnC-WebGUI/MySQL-Docker
 
