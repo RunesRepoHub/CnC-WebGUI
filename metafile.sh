3cfdb1a6a5ae97917ad4082cde88269615cb3b57
rune <christian.rune1998@gmail.com>
2023-09-26 06:06:48 +0200

    Hot fix

--- metafile.sh
+++ metafile.sh
@@ -2,6 +2,6 @@ version="1.2"
 author="Rune004"
 git_repo="https://git.rp-helpdesk.com/Rune/CnC-WebGUI/src/branch/Production"
 
-export version=$version
-export author=$author
-export git_repo=$git_repo
+export version="$version"
+export author="$author"
+export git_repo="$git_repo"
