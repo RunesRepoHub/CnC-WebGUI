2d01a9233fa1e98ef88cd518a6a692e5685a58a5
rune <christian.rune1998@gmail.com>
2023-09-26 15:11:59 +0200

    Testing of version control

--- Download-Agent.sh
+++ Download-Agent.sh
@@ -1,6 +1,15 @@
-rm -r CnC-WebGUI/
+GIT_Version=$("`wget -qO- https://git.rp-helpdesk.com/Rune/CnC-WebGUI/raw/branch/Dev/Version.txt`")
+LOCAL_Version=$(cat ~/CnC-WebGUI/Version.txt)
 
-git clone --branch Production https://git.rp-helpdesk.com/Rune/CnC-WebGUI.git
+if [ $GIT_Version == $LOCAL_Version ]; then 
+    
+    bash ~/CnC-WebGUI/CnC-Agent/Install-Agent.sh
 
-bash ~/CnC-WebGUI/CnC-Agent/Install-Agent.sh
+else 
 
+    rm -r CnC-WebGUI/
+
+    git clone --branch Production https://git.rp-helpdesk.com/Rune/CnC-WebGUI.git
+
+    bash ~/CnC-WebGUI/CnC-Agent/Install-Agent.sh
+fi
