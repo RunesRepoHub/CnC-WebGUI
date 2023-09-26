a0cb86fbe014b540f5c2d85cc4c345d6ab891981
rune <christian.rune1998@gmail.com>
2023-09-26 06:41:51 +0200

    Testing

--- Download-Agent.sh
+++ Download-Agent.sh
@@ -4,5 +4,5 @@ folder="CnC-WebGUI/CnC-Agent"
 
 git clone --branch Production https://git.rp-helpdesk.com/Rune/CnC-WebGUI.git
 
-bash $USERNAME/$folder/Install-Agent.sh
+bash /$USERNAME/$folder/Install-Agent.sh
 
