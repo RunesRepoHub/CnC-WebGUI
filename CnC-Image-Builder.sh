201e0994e90bd468a40537efd3dfaa30b96c371c
rune <christian.rune1998@gmail.com>
2023-09-26 06:10:46 +0200

    Testing

--- CnC-Image-Builder.sh
+++ CnC-Image-Builder.sh
@@ -1,5 +1,5 @@
 USERNAME=$(whoami)
-bash /$USERNAME/CnC-WebGUI/metafile.sh
+version=$(cat /$USERNAME/CnC-WebGUI/Version.txt)
 
 docker build -t cnc-web:$version /$USERNAME/CnC-WebGUI/CnC-WebGUI/Nginx-Docker
 docker build -t cnc-mysql:$version /$USERNAME/CnC-WebGUI/CnC-WebGUI/MySQL-Docker
--- metafile.sh
+++
@@ -1,7 +0,0 @@
-version="1.2"
-author="Rune004"
-git_repo="https://git.rp-helpdesk.com/Rune/CnC-WebGUI/src/branch/Production"
-
-export version="$version"
-export author="$author"
-export git_repo="$git_repo"
