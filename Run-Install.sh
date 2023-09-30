e1b5760b724d430ded94c663a6c62dc572c2742e
rune <christian.rune1998@gmail.com>
2023-09-30 13:58:14 +0200

    Minor edit to disclaimers

--- Run-Install.sh
+++ Run-Install.sh
@@ -3,13 +3,13 @@
 echo "Develop Version is undergoing constant updates and changes to the code"
 echo "And will therefor not always work as it should"
 echo "THIS HAS BEEN YOUR WARNING"
-
+echo 
 echo "The Production Version will only see massive update and changes to the code"
 echo "When it has been tested and vaildated on Debian 9,10,11 and Ubuntu 20.04 22.04"
-
+echo 
 echo '"This "software" is in "early access" so the will be a high likelyness of data loss when updating'
 echo "I will try me best to avoid it, but this is a headsup and warning to backup before updating"
-
+echo 
 PS3="Select the what version you want to install:"
 
 items=("Develop" "Production")
