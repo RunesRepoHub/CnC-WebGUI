1950d7390062889b8e75f1774ada2a5fad4c41c4
rune <christian.rune1998@gmail.com>
2023-09-30 14:44:12 +0200

    Testing

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 mkdir ~/CnC/files
-wget https://git.rp-helpdesk.com/Rune/CnC-WebGUI/raw/branch/Dev/Run-Install.sh ~/CnC/files/
+wget https://git.rp-helpdesk.com/Rune/CnC-WebGUI/raw/branch/Dev/Run-Install.sh 
 
-bash ~/CnC/files/Run-Install.sh
+bash ~/Run-Install.sh
