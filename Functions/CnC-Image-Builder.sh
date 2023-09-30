e85968c8a546f5090c2e7ce61583d734ee14637c
rune <christian.rune1998@gmail.com>
2023-09-30 15:06:53 +0200

    A bit of rollback to remake dockers on install

--- Functions/CnC-Image-Builder.sh
+++ Functions/CnC-Image-Builder.sh
@@ -14,18 +14,6 @@ docker build -t cnc-mysql:$version ~/CnC-WebGUI/CnC-WebGUI/MySQL-Docker >> ~/CnC
 
 clear
 
-if [[ $dockerdb="cnc-mysql:$version" && $dockerweb="cnc-web:$version" ]]; then
-
-    docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui up -d
-    echo " "
-    echo " "
-
-    echo "This server can be used by CnC-Agents via this IP Address: $ip_address"
-
-    echo " "
-    echo " "
-
-else
     docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui down
     docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui up -d
     echo " "
@@ -35,5 +23,3 @@ else
 
     echo " "
     echo " "
-
-fi 
