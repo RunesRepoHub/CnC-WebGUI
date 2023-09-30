2055c60b5dc519801b7624e632fef59ae98243a4
rune <christian.rune1998@gmail.com>
2023-09-30 16:56:16 +0200

    Testing

--- Functions/Server/Install-Develop.sh
+++ Functions/Server/Install-Develop.sh
@@ -1,15 +1,15 @@
 ## Get Version from repo 
 GIT_Version=$(wget -qO- https://git.rp-helpdesk.com/Rune/CnC-WebGUI/raw/branch/Dev/Functions/Version.txt)
 ## Get Local Version
-LOCAL_Version=$(cat ~/CnC-WebGUI/Functions/Version.txt)
+LOCAL_Version=$(cat /home/$userprofile/CnC-WebGUI/Functions/Version.txt)
 
-user=$(id -u -n)
+userprofile=$(id -u -n)
 
 ## Compare Local and repo versions
 if [ $GIT_Version == "$LOCAL_Version" ]; then
 ## If local version is up to date with repo just run:
 
-    bash /home/$user/CnC-WebGUI/Functions/Server/Getting-started.sh
+    bash /home/$userprofile/CnC-WebGUI/Functions/Server/Getting-started.sh
 
 elif [ $GIT_Version > "$LOCAL_Version" ] ; then 
 
@@ -27,12 +27,12 @@ elif [ $GIT_Version > "$LOCAL_Version" ] ; then
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
         # do dangerous stuff
-        wget https://git.rp-helpdesk.com/Rune/CnC-WebGUI/raw/branch/Production/Functions/Server/Update.sh -P ~/ > /dev/null 2>&1
-        bash /home/$user/CnC-WebGUI/Functions/Server/Update.sh
+        wget https://git.rp-helpdesk.com/Rune/CnC-WebGUI/raw/branch/Production/Functions/Server/Update.sh -P /home/$userprofile/ > /dev/null 2>&1
+        bash /home/$userprofile/CnC-WebGUI/Functions/Server/Update.sh
 
         sleep 5
 
-        bash /home/$user/CnC-WebGUI/Functions/Server/Getting-started.sh
+        bash /home/$userprofile/CnC-WebGUI/Functions/Server/Getting-started.sh
     fi 
 else 
     echo "An Error was incountered"
