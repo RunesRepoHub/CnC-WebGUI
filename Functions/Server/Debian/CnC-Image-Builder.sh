03e47c8676858116935f845d321e4e63a8a56395
rune <christian.rune1998@gmail.com>
2023-10-12 00:29:28 +0200

    Minor clean and patch work

--- Functions/Server/Debian/CnC-Image-Builder.sh
+++ Functions/Server/Debian/CnC-Image-Builder.sh
@@ -5,24 +5,26 @@ dockerdb=$(docker ps | grep -i cnc-mysql: | awk '{print $2}')
 dockerweb=$(docker ps | grep -i cnc-web: | awk '{print $2}')
 
 
+docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui down
+
+clear
+
 
 touch ~/CnC-WebGUI/CnC-WebGUI/.env
 echo "version=$version" > ~/CnC-WebGUI/CnC-WebGUI/.env
 
 
-docker network create -d bridge cncnetwork  >> ~/CnC-WebGUI/Logs/CnC-Image-Builder.log
-docker build -t cnc-web:$version /root/Development/CnC-WebGUI/CnC-WebGUI/Nginx-Docker  >> ~/CnC-WebGUI/Logs/CnC-Image-Builder.log
-docker build -t cnc-pg:$version /root/Development/CnC-WebGUI/CnC-WebGUI/Postgress-Docker  >> ~/CnC-WebGUI/Logs/CnC-Image-Builder.log
-docker build -t cnc-node-api:$version /root/Development/CnC-WebGUI/CnC-WebGUI/Nodejs-Docker  >> ~/CnC-WebGUI/Logs/CnC-Image-Builder.log
+docker network create -d bridge cncnetwork
+docker build -t cnc-web:$version /root/Development/CnC-WebGUI/CnC-WebGUI/Nginx-Docke
+docker build -t cnc-pg:$version /root/Development/CnC-WebGUI/CnC-WebGUI/Postgress-Docker
+docker build -t cnc-node-api:$version /root/Development/CnC-WebGUI/CnC-WebGUI/Nodejs-Docker
 
 clear
 
-    docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui down
-    docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui up -d
-    echo " "
-    echo " "
 
-    echo "This server can be used by CnC-Agents via this IP Address: $ip_address"
-
-    echo " "
-    echo " "
+docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui up -d
+echo " "
+echo " "
+echo "This server can be used by CnC-Agents via this IP Address: $ip_address"
+echo " "
+echo " "
