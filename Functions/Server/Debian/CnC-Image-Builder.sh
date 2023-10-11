2196538f7b8c7dd25fe5fc094364aeb39242f844
rune <christian.rune1998@gmail.com>
2023-10-12 00:43:27 +0200

    Minor fix after version tag change

--- Functions/Server/Debian/CnC-Image-Builder.sh
+++ Functions/Server/Debian/CnC-Image-Builder.sh
@@ -1,34 +1,22 @@
-version=$(cat ~/CnC-WebGUI/CnC-WebGUI/.env)
+version=$(cat ~/CnC-WebGUI/CnC-WebGUI/.env | awk '{ print substr( $0, 9 ) }')
 ip_address=$(hostname -I | awk '{print $1}')
 
 dockerdb=$(docker ps | grep -i cnc-mysql: | awk '{print $2}')
 dockerweb=$(docker ps | grep -i cnc-web: | awk '{print $2}')
 
 
-
-touch ~/CnC-WebGUI/CnC-WebGUI/.env
-echo "version=$version" > ~/CnC-WebGUI/CnC-WebGUI/.env
-
-
 docker network create -d bridge cncnetwork
 docker build -t cnc-web:$version ~/CnC-WebGUI/CnC-WebGUI/Nginx-Docker
-
 docker build -t cnc-pg:$version ~/CnC-WebGUI/CnC-WebGUI/Postgress-Docker
-
 docker build -t cnc-node-api:$version ~/CnC-WebGUI/CnC-WebGUI/Nodejs-Docker
-
-
-
-
 
 clear
 
-    docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui down
-    docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui up -d
-    echo " "
-    echo " "
+docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui down
+docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui up -d
 
-    echo "This server can be used by CnC-Agents via this IP Address: $ip_address"
-
-    echo " "
-    echo " "
+echo " "
+echo " "
+echo "This server can be used by CnC-Agents via this IP Address: $ip_address"
+echo " "
+echo " "
