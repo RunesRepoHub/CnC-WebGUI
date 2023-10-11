dee72e12cb4a5a169056b7c1f72203fa7e9b7f49
rune <christian.rune1998@gmail.com>
2023-10-11 23:25:37 +0200

    Added docker auto install

--- Functions/Server/Check-OS.sh
+++ Functions/Server/Check-OS.sh
@@ -1,4 +1,67 @@
-apt-get install git 
+#!/bin/bash
+
+apt-get update
+apt-get upgrade -y
+apt-get install git
+
+# Function to install Docker CLI
+install_docker_cli() {
+  # Update the package list and install required packages
+  sudo apt update
+  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+  # Add Docker GPG key and repository
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  # Install Docker
+  sudo apt update
+  sudo apt install -y docker-ce
+
+  # Start and enable the Docker service
+  sudo systemctl start docker
+  sudo systemctl enable docker
+
+  # Add the current user to the "docker" group to run Docker without sudo
+  sudo usermod -aG docker $USER
+
+  # Output Docker version
+  docker --version
+}
+
+# Function to install Docker Compose
+install_docker_compose() {
+  # Download the latest stable release of Docker Compose
+  sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+  # Make Docker Compose executable
+  sudo chmod +x /usr/local/bin/docker-compose
+
+  # Output Docker Compose version
+  docker-compose --version
+}
+
+# Check the Linux distribution and version
+if [ -f /etc/os-release ]; then
+  . /etc/os-release
+  if [[ $ID == "debian" && ($VERSION_ID == "9" || $VERSION_ID == "10" || $VERSION_ID == "11") ]]; then
+    install_docker_cli
+    install_docker_compose
+  elif [[ $ID == "ubuntu" && $VERSION_ID == "22.04" ]]; then
+    install_docker_cli
+    install_docker_compose
+  else
+    echo "Unsupported distribution or version."
+    exit 1
+  fi
+else
+  echo "Unable to determine the Linux distribution and version."
+  exit 1
+fi
+
+echo "Docker CLI and Docker Compose installation completed."
+
+
 if [ -f /etc/os-release ]; then
     # freedesktop.org and systemd
     . /etc/os-release
