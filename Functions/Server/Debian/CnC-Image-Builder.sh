df83c2c0f4821c2853af2f8572700ea7f106da76
rune <christian.rune1998@gmail.com>
2023-10-12 00:35:25 +0200

    Changed the version tag method

--- Functions/Server/Debian/CnC-Image-Builder.sh
+++ Functions/Server/Debian/CnC-Image-Builder.sh
@@ -1,13 +1,9 @@
-version=$(cat ~/CnC-WebGUI/Functions/Version.txt)
+version=$(cat ~/CnC-WebGUI/CnC-WebGUI/.env)
 ip_address=$(hostname -I | awk '{print $1}')
 
 dockerdb=$(docker ps | grep -i cnc-mysql: | awk '{print $2}')
 dockerweb=$(docker ps | grep -i cnc-web: | awk '{print $2}')
 
-
-docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui down
-
-clear
 
 
 touch ~/CnC-WebGUI/CnC-WebGUI/.env
@@ -15,16 +11,24 @@ echo "version=$version" > ~/CnC-WebGUI/CnC-WebGUI/.env
 
 
 docker network create -d bridge cncnetwork
-docker build -t cnc-web:1.2 /root/Development/CnC-WebGUI/CnC-WebGUI/Nginx-Docke
-docker build -t cnc-pg:1.2 /root/Development/CnC-WebGUI/CnC-WebGUI/Postgress-Docker
-docker build -t cnc-node-api:1.2 /root/Development/CnC-WebGUI/CnC-WebGUI/Nodejs-Docker
+docker build -t cnc-web:$version ~/CnC-WebGUI/CnC-WebGUI/Nginx-Docker
+
+docker build -t cnc-pg:$version ~/CnC-WebGUI/CnC-WebGUI/Postgress-Docker
+
+docker build -t cnc-node-api:$version ~/CnC-WebGUI/CnC-WebGUI/Nodejs-Docker
+
+
+
+
 
 clear
 
+    docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui down
+    docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui up -d
+    echo " "
+    echo " "
 
-docker compose -f ~/CnC-WebGUI/CnC-WebGUI/docker-compose.yaml -p cnc-webgui up -d
-echo " "
-echo " "
-echo "This server can be used by CnC-Agents via this IP Address: $ip_address"
-echo " "
-echo " "
+    echo "This server can be used by CnC-Agents via this IP Address: $ip_address"
+
+    echo " "
+    echo " "
