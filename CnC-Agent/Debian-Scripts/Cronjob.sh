99f0dc949403fd0bf4c52b4da30ec3c0d6195467
rune <christian.rune1998@gmail.com>
2023-10-12 10:49:18 +0200

    Meta path file  #14

--- CnC-Agent/Debian-Scripts/Cronjob.sh
+++ CnC-Agent/Debian-Scripts/Cronjob.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+source ~/CnC-WebGUI/config.sh
+
 databaseip=$(cat ~/CnC-WebGUI/CnC-Agent/.databaseip)
 hn=$(echo $HOSTNAME)
 me=$(basename "$0")
 
 # Capture the user's crontab and save it to a file
-crontab -l > ~/CnC-WebGUI/CnC-Agent/cron.txt
+crontab -l > $crontxt
 
 # Process each line of the crontab
 while IFS= read -r line; do
     # Send the cron job data to the database
     curl -X POST -H "Content-Type: application/json" -d "{\"hostname\": \"$hn\", \"cronjobsscripts\": \"$line\"}" "http://$databaseip:3000/create/cronjobs"
-done < ~/CnC-WebGUI/CnC-Agent/cron.txt
+done < $crontxt
--- config.sh
+++ config.sh
@@ -4,3 +4,7 @@
 CnC_Image_Builder="bash ~/CnC-WebGUI/Functions/CnC-Image-Builder.sh"
 Install_Agent="bash ~/CnC-WebGUI/CnC-Agent/Install-Agent.sh"
 Uninstall=""
+
+
+# Cronjob.sh 
+crontxt="~/CnC-WebGUI/CnC-Agent/cron.txt"
