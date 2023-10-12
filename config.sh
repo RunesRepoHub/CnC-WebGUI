b59c6f19063401d3d9cb195bcd3bc73990b9ccef
rune <christian.rune1998@gmail.com>
2023-10-12 10:38:20 +0200

    Meta path file  #14

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Getting-started.sh
-CnC_Image_Builder="~/CnC-WebGUI/Functions/CnC-Image-Builder.sh"
-Install_Agent="~/CnC-WebGUI/CnC-Agent/Install-Agent.sh"
+CnC_Image_Builder="bash ~/CnC-WebGUI/Functions/CnC-Image-Builder.sh"
+Install_Agent="bash ~/CnC-WebGUI/CnC-Agent/Install-Agent.sh"
 Uninstall=""
