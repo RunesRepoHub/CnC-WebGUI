fc2d1899bf488415b55fd7a4fc5a1807525c9537
rune <christian.rune1998@gmail.com>
2023-10-12 10:27:11 +0200

    Meta path file  #14

--- Functions/Getting-started.sh
+++ Functions/Getting-started.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Source the configuration script
+source ~/CnC-WebGUI/config.sh
+
 clear 
 PS3="Select the what you want to install on this server:"
 
@@ -7,9 +10,9 @@ while true; do
     select item in "${items[@]}" Quit
     do
         case $REPLY in
-            1) bash ~/CnC-WebGUI/Functions/CnC-Image-Builder.sh; break;;
-            2) bash ~/CnC-WebGUI/CnC-Agent/Install-Agent.sh; break 2;;
-            3) bash ~/CnC-WebGUI/Functions/Uninstall.sh; break 2;;
+            1) bash $func_dir/CnC-Image-Builder.sh; break;;
+            2) bash $agen_dir/Install-Agent.sh; break 2;;
+            3) bash $func_dir/Uninstall.sh; break 2;;
             $((${#items[@]}+1))) echo "We're done!"; break 2;;
             *) echo "Ooops - unknown choice $REPLY"; break;
         esac
---
+++ config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Getting-started.sh
+func_dir="~/CnC-WebGUI/Functions/CnC-Image-Builder.sh"
+agen_dir="~/CnC-WebGUI/CnC-Agent/"
+
+# Define file paths
+data_dir="/path/to/data"
+log_dir="/path/to/logs"
+config_file="/path/to/config.conf"
