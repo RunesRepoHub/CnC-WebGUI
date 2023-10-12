3e0848f8ea211db3829ac57236b3a7d6aaa193d2
rune <christian.rune1998@gmail.com>
2023-10-12 10:45:24 +0200

    Meta path file  #14

--- Functions/Getting-started.sh
+++ Functions/Getting-started.sh
@@ -11,9 +11,9 @@ while true; do
     select item in "${items[@]}" Quit
     do
         case $REPLY in
-            1) source "$CnC_Image_Builder"; break;;
-            2) source "$Install_Agent"; break 2;;
-            3) source "$Uninstall"; break 2;;
+            1) eval "$CnC_Image_Builder"; break;;  # Use `eval` to execute the command in the variable.
+            2) eval "$Install_Agent"; break 2;;  # Use `eval` to execute the command in the variable.
+            3) eval "$Uninstall"; break 2;;  # Use `eval` to execute the command in the variable.
             $((${#items[@]}+1))) echo "We're done!"; break 2;;
             *) echo "Oops - unknown choice $REPLY"; break;
         esac
