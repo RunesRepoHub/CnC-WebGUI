732edc25f0fb089cde2c0598c08840bf03b72868
rune <christian.rune1998@gmail.com>
2023-10-12 10:53:26 +0200

    Hotfix

--- CnC-Agent/Debian-Scripts/Cronjob.sh
+++ CnC-Agent/Debian-Scripts/Cronjob.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 source ~/CnC-WebGUI/config.sh
 
-databaseip=$(cat source "$dbip")
+databaseip=$(cat eval "$dbip")
 hn=$(echo $HOSTNAME)
 me=$(basename "$0")
 
 # Capture the user's crontab and save it to a file
-crontab -l > $crontxt
+crontab -l > eval "$crontxt"
 
 # Process each line of the crontab
 while IFS= read -r line; do
     # Send the cron job data to the database
     curl -X POST -H "Content-Type: application/json" -d "{\"hostname\": \"$hn\", \"cronjobsscripts\": \"$line\"}" "http://$databaseip:3000/create/cronjobs"
-done < source "$crontxt"
+done < eval "$crontxt"
