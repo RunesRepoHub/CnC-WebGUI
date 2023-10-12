4d0eb1a0ee824254b22c42fdd5e02e244faf9f5f
rune <christian.rune1998@gmail.com>
2023-10-12 10:36:49 +0200

    Meta path file  #14

--- Functions/Getting-started.sh
+++ Functions/Getting-started.sh
@@ -10,9 +10,9 @@ while true; do
     select item in "${items[@]}" Quit
     do
         case $REPLY in
-            1) bash $func_dir; break;;
-            2) bash $agen_dir; break 2;;
-            3) bash $func_dir/Uninstall.sh; break 2;;
+            1) bash $CnC_Image_Builder; break;;
+            2) bash $Install_Agent; break 2;;
+            3) bash $Uninstall; break 2;;
             $((${#items[@]}+1))) echo "We're done!"; break 2;;
             *) echo "Ooops - unknown choice $REPLY"; break;
         esac
--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Getting-started.sh
-CnC-Image-Builder="~/CnC-WebGUI/Functions/CnC-Image-Builder.sh"
-Install-Agent="~/CnC-WebGUI/CnC-Agent/Install-Agent.sh"
+CnC_Image_Builder="~/CnC-WebGUI/Functions/CnC-Image-Builder.sh"
+Install_Agent="~/CnC-WebGUI/CnC-Agent/Install-Agent.sh"
 Uninstall=""
