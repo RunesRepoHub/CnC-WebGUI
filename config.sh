3b782a5fa6533214e8a1610d9250fe34841cab12
rune <christian.rune1998@gmail.com>
2023-10-12 11:55:38 +0200

    Hotfix testing

--- config.sh
+++ config.sh
@@ -10,8 +10,8 @@ echo "$Version" > "$ver_path"
 dbip="/root/CnC-WebGUI/CnC-Agent/.databaseip"
 
 # Getting-started.sh
-CnC_Image_Builder="bash ~/CnC-WebGUI/Functions/CnC-Image-Builder.sh"
-Install_Agent="bash ~/CnC-WebGUI/CnC-Agent/Install-Agent.sh"
+CnC_Image_Builder="bash /root/CnC-WebGUI/Functions/CnC-Image-Builder.sh"
+Install_Agent="bash /root/CnC-WebGUI/CnC-Agent/Install-Agent.sh"
 Uninstall=""
 
 # Cronjob.sh 
