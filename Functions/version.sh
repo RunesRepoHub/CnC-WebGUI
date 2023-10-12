6bb250bbca53e5f87ad86c8eb2e7b5e7e3a30171
rune <christian.rune1998@gmail.com>
2023-10-12 12:42:22 +0200

    Testing

--- Functions/version.sh
+++ Functions/version.sh
@@ -1,4 +1,4 @@
 source ~/CnC-WebGUI/config.sh
-touch ~/CnC-WebGUI/CnC-WebGUI/.env
-echo source "$Version" > ~/CnC-WebGUI/CnC-WebGUI/.env
+touch /root/CnC-WebGUI/CnC-WebGUI/.env
+echo source "$Version" > /root/CnC-WebGUI/CnC-WebGUI/.env
 
