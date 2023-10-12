705e0faa57c32becbe735ec83a4e1e3e202526a7
rune <christian.rune1998@gmail.com>
2023-10-12 11:57:53 +0200

    Testing fix

--- Functions/Getting-started.sh
+++ Functions/Getting-started.sh
@@ -11,9 +11,9 @@ while true; do
     select item in "${items[@]}" Quit
     do
         case $REPLY in
-            1) eval "$CnC_Image_Builder"; break;;  # Use `eval` to execute the command in the variable.
-            2) eval "$Install_Agent"; break 2;;  # Use `eval` to execute the command in the variable.
-            3) eval "$Uninstall"; break 2;;  # Use `eval` to execute the command in the variable.
+            1) bash "$CnC_Image_Builder"; break;;  
+            2) bash "$Install_Agent"; break 2;;  
+            3) bash "$Uninstall"; break 2;;  
             $((${#items[@]}+1))) echo "We're done!"; break 2;;
             *) echo "Oops - unknown choice $REPLY"; break;
         esac
