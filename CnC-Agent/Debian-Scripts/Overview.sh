f36a5e120f90566a9a2eaa373d3a4b44cb936c49
rune <christian.rune1998@gmail.com>
2023-10-14 00:25:09 +0200

    Testing overview.sh

--- CnC-Agent/Debian-Scripts/Overview.sh
+++ CnC-Agent/Debian-Scripts/Overview.sh
@@ -5,70 +5,77 @@ source ~/CnC-WebGUI/config.sh
 me=$(basename "$0")
 databaseip=$(cat "$dbip")
 
+# Set the hostname variable
 hostname=$(echo $HOSTNAME)
 
-ip_address=$(hostname -I | awk '{print $1}')
+# Define the URL for reading data
+READ_API_ENDPOINT="http://192.168.1.169:3000/read/info/$hostname"
 
-mac_address=$(cat /sys/class/net/*/address | sed -n '1 p')
+# Fetch existing data from the API
+existing_data=$(curl -s "$READ_API_ENDPOINT")
 
-packages=$(apt-get -q -y --ignore-hold --allow-change-held-packages --allow-unauthenticated -s dist-upgrade | /bin/grep  ^Inst | wc -l)
+if [ -n "$existing_data" ]; then
+  echo "Data for hostname $hostname exists. Updating..."
+  # Extract existing data and compare with new data
+  existing_ip_address=$(echo "$existing_data" | jq -r .ipaddress)
+  existing_mac_address=$(echo "$existing_data" | jq -r .macaddress)
+  existing_distro=$(echo "$existing_data" | jq -r .distro)
+  existing_packages=$(echo "$existing_data" | jq -r .packages)
 
-if [ -f /etc/os-release ]; then
-    # freedesktop.org and systemd
-    . /etc/os-release
-    OS=$NAME
-    VER=$VERSION_ID
-elif type lsb_release >/dev/null 2>&1; then
-    # linuxbase.org
-    OS=$(lsb_release -si)
-    VER=$(lsb_release -sr)
-elif [ -f /etc/lsb-release ]; then
-    # For some versions of Debian/Ubuntu without lsb_release command
-    . /etc/lsb-release
-    OS=$DISTRIB_ID
-    VER=$DISTRIB_RELEASE
-elif [ -f /etc/debian_version ]; then
-    # Older Debian/Ubuntu/etc.
-    OS=Debian
-    VER=$(cat /etc/debian_version)
-elif [ -f /etc/SuSe-release ]; then
-    # Older SuSE/etc.
-    ...
-elif [ -f /etc/redhat-release ]; then
-    # Older Red Hat, CentOS, etc.
-    ...
+  # Compare existing data with new data
+  if [ "$existing_ip_address" != "$ip_address" ]; then
+    ip_address="$existing_ip_address"
+  fi
+
+  if [ "$existing_mac_address" != "$mac_address" ]; then
+    mac_address="$existing_mac_address"
+  fi
+
+  if [ "$existing_distro" != "$distro" ]; then
+    distro="$existing_distro"
+  fi
+
+  if [ "$existing_packages" != "$packages" ]; then
+    packages="$existing_packages"
+  fi
+
+  # Prepare the data for update
+  DATA='{
+      "hostname": "'"$hostname"'",
+      "ipaddress": "'"$ip_address"'",
+      "macaddress": "'"$mac_address"'",
+      "distro": "'"$distro"'",
+      "packages": "'"$packages"'"
+  }'
+
+  # Send a PUT request to update the data
+  UPDATE_API_ENDPOINT="http://192.168.1.169:3000/update/info/$hostname"
+  response=$(curl -X PUT -H "Content-Type: application/json" -d "$DATA" "$UPDATE_API_ENDPOINT")
+
+  if [ "$response" == "Data updated" ]; then
+    echo "Data updated from $me."
+  else
+    echo "Data update failed."
+  fi
 else
-    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
-    OS=$(uname -s)
-    VER=$(uname -r)
+  echo "Data for hostname $hostname does not exist. Inserting..."
+
+  # Prepare the data for insertion
+  DATA='{
+      "hostname": "'"$hostname"'",
+      "ipaddress": "'"$ip_address"'",
+      "macaddress": "'"$mac_address"'",
+      "distro": "'"$distro"'",
+      "packages": "'"$packages"'"
+  }'
+
+  # Send a POST request to insert the data
+  CREATE_API_ENDPOINT="http://192.168.1.169:3000/create/info"
+  response=$(curl -X POST -H "Content-Type: application/json" -d "$DATA" "$CREATE_API_ENDPOINT")
+
+  if [ "$response" == "Data inserted" ]; then
+    echo "Data inserted from $me."
+  else
+    echo "Data insert failed."
+  fi
 fi
-
-distro="$OS $VER"
-
-# Define your REST API endpoint for updating/inserting data
-API_ENDPOINT="http://$databaseip:3000/create/info"
-
-# Define the data to be sent to the API
-DATA='{
-    "hostname": "'"$hostname"'",
-    "ipaddress": "'"$ip_address"'",
-    "macaddress": "'"$mac_address"'",
-    "distro": "'"$distro"'",
-    "packages": "'"$packages"'"
-}'
-
-# Debugging: Print the data being sent
-#echo "Sending data: $DATA"
-
-# Send a POST request to the API to update or insert data
-response=$(curl -X POST -H "Content-Type: application/json" -d "$DATA" "$API_ENDPOINT" >/dev/null 2>&1)
-
-# Debugging: Print the response from the API
-#echo "API response: $response"
-
-# Check the response from the API
-# if [ "$response" == "Data updated" ]; then
-#     echo "Data updated from $me."
-# else
-#     echo "Data inserted from $me."
-# fi
