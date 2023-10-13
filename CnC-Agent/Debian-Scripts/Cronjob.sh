4a8f56bebd7205b7f1fcc48350121ebd1f2dfb25
rune <christian.rune1998@gmail.com>
2023-10-14 01:44:57 +0200

    Testing

--- CnC-Agent/Debian-Scripts/Cronjob.sh
+++ CnC-Agent/Debian-Scripts/Cronjob.sh
@@ -9,17 +9,17 @@ me=$(basename "$0")
 # Capture the user's crontab and save it to a file
 crontab -l > "$crontxt"
 
-# Fetch existing data from the URL and strip any leading/trailing whitespaces
-existing_data=$(curl -s "http://192.168.1.169:3000/read/cronjobs/$hn" | tr -d '[:space:]')
+# Fetch existing data from the URL
+existing_data=$(curl -s "http://192.168.1.169:3000/read/cronjobs/$hn")
+
+# Extract the "cronjobsscripts" field from the existing data
+existing_cronjobs=$(echo "$existing_data" | jq -r '.cronjobsscripts')
 
 # Process each line of the crontab
 while IFS= read -r line; do
-    # Strip any leading/trailing whitespaces from the line
-    line=$(echo "$line" | tr -d '[:space:]')
-
-    # Check if the line exists in the existing data
-    if [[ "$existing_data" != *"$line"* ]]; then
-        # Line does not exist in existing data, so add it
-        curl -X POST -H "Content-Type: application/json" -d "{\"hostname\": \"$hn\", \"cronjobsscripts\": \"$line\"}" "http://$databaseip:3000/create/cronjobs" >/dev/null 2>&1
+    # Check if the line exists in the existing cron jobs
+    if [[ "$existing_cronjobs" != *"$line"* ]]; then
+        # Line does not exist in existing cron jobs, so add it
+        curl -X POST -H "Content-Type: application/json" -d "{\"hostname\": \"$hn\", \"cronjobsscripts\": \"$line\"}" "http://$databaseip:3000/create/cronjobs" >/devnull 2>&1
     fi
 done < "$crontxt"
