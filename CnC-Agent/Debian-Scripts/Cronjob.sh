9201bf176f54c442fdcd5bdec2d3a2db266df031
rune <christian.rune1998@gmail.com>
2023-10-14 01:13:27 +0200

    Testing

--- CnC-Agent/Debian-Scripts/Cronjob.sh
+++ CnC-Agent/Debian-Scripts/Cronjob.sh
@@ -5,10 +5,9 @@ source ~/CnC-WebGUI/config.sh
 databaseip=$(cat "$dbip")
 hn=$(echo $HOSTNAME)
 me=$(basename "$0")
-id=1  # Replace this with the desired id
 
 # Fetch existing data from the API based on "id"
-existing_data=$(curl -s "http://192.168.1.169:3000/read/cronjobs/$id")
+existing_data=$(curl -s "http://192.168.1.169:3000/read/cronjobs/$hn")
 
 # Get the user's crontab
 user_crontab=$(crontab -l)
@@ -29,7 +28,7 @@ if [ -n "$existing_data" ]; then
     # Compare existing data with new data
     if [ "$existing_cronjobs" != "$user_crontab" ]; then
         # Send a PUT request to update the data
-        response=$(curl -X PUT -H "Content-Type: application/json" -d "$DATA" "http://192.168.1.169:3000/update/cronjobs/$id")
+        response=$(curl -X PUT -H "Content-Type: application/json" -d "$DATA" "http://192.168.1.169:3000/update/cronjobs/$hn")
 
         if [ "$response" == "Data updated" ]; then
             echo "Data updated from $me."
