eeb4a816bf3c8e2d93857841f5bb9671d1c446bb
rune <christian.rune1998@gmail.com>
2023-10-14 01:21:19 +0200

    Testing

--- CnC-Agent/Debian-Scripts/Cronjob.sh
+++ CnC-Agent/Debian-Scripts/Cronjob.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
+
+# Define the URL to fetch data from
+data_url="http://192.168.1.169:3000/read/cronjobs/$hostname"
+
 # Source the configuration script
 source ~/CnC-WebGUI/config.sh
 
+# Define your database IP, hostname, and other variables
 databaseip=$(cat "$dbip")
 hn=$(echo $HOSTNAME)
 me=$(basename "$0")
 
-# Function to update variables from API response
-update_variables() {
-    local data="$1"
-    ip_address=$(jq -r '.ip_address' <<< "$data")
-    mac_address=$(jq -r '.mac_address' <<< "$data")
-    distro=$(jq -r '.distro' <<< "$data")
-    packages=$(jq -r '.packages' <<< "$data")
-}
-
-# Read data from the API and update variables
-response=$(curl -s "http://192.168.1.169:3000/read/cronjobs/$hn")
-if [[ $? -eq 0 ]]; then
-    update_variables "$response"
-else
-    echo "Failed to fetch data from the API."
-    exit 1
-fi
-
 # Capture the user's crontab and save it to a file
 crontab -l > "$crontxt"
 
-# Process each line of the crontab
-while IFS= read -r line; do
-    # Send the cron job data to the database
-    curl -X POST -H "Content-Type: application/json" -d "{\"hostname\": \"$hn\", \"cronjobsscripts\": \"$line\"}" "http://$databaseip:3000/create/cronjobs" >/dev/null 2>&1
-done < "$crontxt"
+# Fetch the data from the URL
+data=$(curl -s "$data_url")
+
+# Check if the data is not empty
+if [ -n "$data" ]; then
+    # Compare the data to the hostname
+    if [ "$data" = "$hn" ]; then
+        # Process each line of the crontab
+        while IFS= read -r line; do
+            # Send the cron job data to the database
+            curl -X POST -H "Content-Type: application/json" -d "{\"hostname\": \"$hn\", \"cronjobsscripts\": \"$line\"}" "http://$databaseip:3000/update/cronjobs/$id" >/dev/null 2>&1
+        done < "$crontxt"
+    else
+        echo "Hostname does not match data from the URL."
+    fi
+else
+    echo "Failed to fetch data from the URL: $data_url"
+fi
