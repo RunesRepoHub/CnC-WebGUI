acbe6c1da87bf2630639bcfb7df54b0afc137f51
rune <christian.rune1998@gmail.com>
2023-10-13 23:58:17 +0200

    Updating installation scripts

--- Functions/Run-Install-Debian.sh
+++ Functions/Run-Install-Debian.sh
@@ -3,20 +3,18 @@ echo
 echo
 echo "---------------------------------------------------------------"
 echo "The Development Version is undergoing constant updates and changes to the code and will therefor not always work as it should... THIS HAS BEEN YOUR WARNING"
-echo
-echo "---------------------------------------------------------------"
-echo 
+echo "--------------------------------------------------------------" 
 echo "The Production Version will only see massive update and changes to the code" 
 echo "When it has been tested and vaildated on:"
 echo
 echo " * Debian 9,10,11"
 echo
-echo " * Ubuntu 20.04,22.04"
-echo 
+echo " * Ubuntu 20.04,22.04" 
 echo "---------------------------------------------------------------"
 echo 'This "software" is in "early access" so the will be a high likelyness of data loss when updating I will try me best to avoid it, but this is a headsup and warning to backup before updating'
-echo 
 echo
+
+
 PS3="Select the what version you want to install:"
 
 items=("Development" "Production")
