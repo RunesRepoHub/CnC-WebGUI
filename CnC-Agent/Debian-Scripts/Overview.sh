ca5ae86d297448cf442e09d16bd579ef940acb29
rune <christian.rune1998@gmail.com>
2023-10-13 23:13:15 +0200

    Testing overview.sh

--- CnC-Agent/Debian-Scripts/Overview.sh
+++ CnC-Agent/Debian-Scripts/Overview.sh
@@ -2,24 +2,10 @@
 # Source the configuration script
 source ~/CnC-WebGUI/config.sh
 
-ip_address=$(hostname -I | awk '{print $1}')
-mac_address=$(cat /sys/class/net/*/address | sed -n '1 p')
-packages=$(apt-get -q -y --ignore-hold --allow-change-held-packages --allow-unauthenticated -s dist-upgrade | /bin/grep  ^Inst | wc -l)
 me=$(basename "$0")
 databaseip=$(cat "$dbip")
 
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    OS=$NAME
-    VER=$VERSION_ID
-# ... (other OS detection code)
-
-distro="$OS $VER"
-fi
-
 hostname=$(echo $HOSTNAME)
-
-tpdata='{"id": " ","hostname":"'$hostname'"}
 
 # Define your REST API endpoints
 INFO_ENDPOINT="http://$databaseip:3000/read/info/$hostname"
@@ -46,7 +32,7 @@ if [ -n "$existing_data" ]; then
   # Send a PUT request to update the data
   response=$(curl -X PUT -H "Content-Type: application/json" -d "$DATA" "$INFO_ENDPOINT")
 
-  if [ "$response" == "$DATA" ]; then
+  if [ "$response" == "Data updated" ]; then
     echo "Data updated from $me."
   else
     echo "Data update failed."
@@ -79,7 +65,7 @@ else
   # Send a POST request to insert the data
   response=$(curl -X POST -H "Content-Type: application/json" -d "$DATA" "$CREATE_INFO_ENDPOINT")
 
-  if [ "$response" == "$DATA" ]; then
+  if [ "$response" == "Data inserted" ]; then
     echo "Data inserted from $me."
   else
     echo "Data insert failed."
