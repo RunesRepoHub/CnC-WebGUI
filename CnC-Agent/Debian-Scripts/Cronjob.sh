6943193b1016eb770d4c628d1b96b33677cd430c
rune <christian.rune1998@gmail.com>
2023-10-14 02:00:03 +0200

    Testing

--- CnC-Agent/Debian-Scripts/Cronjob.sh
+++ CnC-Agent/Debian-Scripts/Cronjob.sh
@@ -6,7 +6,7 @@ databaseip=$(cat "$dbip")
 hn=$(echo $HOSTNAME)
 me=$(basename "$0")
 
-# Capture the user's crontab and save it to a file
+# Capture the user's crontab and save it to a temporary file
 crontab -l > "$crontxt"
 
 # Fetch existing data from the URL
@@ -19,14 +19,20 @@ existing_cronjobs=$(echo "$existing_data" | jq -r '.cronjobsscripts')
 echo "$existing_cronjobs" > "$existing_cronjobs_file"
 
 # Use 'diff' to compare the crontab with existing data
-if diff -q "$crontxt" "$existing_cronjobs_file" >/dev/null; then
-    echo "Cron jobs are up to date. No changes required."
-else
+if [ -s "$crontxt" ] && ! diff -q "$crontxt" "$existing_cronjobs_file" >/dev/null; then
     echo "Updating cron jobs..."
     
     # Update the database with the new crontab
-    curl -X POST -H "Content-Type: application/json" -d "{\"hostname\": \"$hn\", \"cronjobsscripts\": \"$(cat "$crontxt")\"}" "http://$databaseip:3000/update/cronjobs/$id" >/dev/null 2>&1
+    update_response=$(curl -X POST -H "Content-Type: application/json" -d "{\"hostname\": \"$hn\", \"cronjobsscripts\": \"$(cat "$crontxt")\"}" "http://$databaseip:3000/update/cronjobs/$id")
+    
+    if [ "$update_response" = "Data update failed." ]; then
+        echo "Error: Data update failed."
+    else
+        echo "Cron jobs updated successfully."
+    fi
+else
+    echo "Cron jobs are up to date. No changes required."
 fi
 
-# Clean up the temporary file
-rm -f "$existing_cronjobs_file"
+# Clean up the temporary files
+rm -f "$existing_cronjobs_file" "$crontxt"
