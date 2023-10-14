d1552c74b8f72fcc8ec397434670e828694c09f9
rune <christian.rune1998@gmail.com>
2023-10-14 14:18:04 +0200

    Testing agent installer

--- Functions/Get-agent-install-script.sh
+++ Functions/Get-agent-install-script.sh
@@ -15,95 +15,41 @@ echo 'This "software" is in "early access" so the will be a high likelyness of d
 echo
 
 
-PS3="Select the what version you want to install:"
+# URL of the GitHub repository and the target directory
+repo_url="https://github.com/RunesRepoHub/CnC-WebGUI.git"
+directory_path="CnC-Agent"
 
-items=("Development" "Production")
+# Clone the repository
+git clone "$repo_url" /tmp/CnC-WebGUI
 
-## Select Version of code to download
+# Check if the cloning was successful
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to clone the repository."
+  exit 1
+fi
 
-while true; do
-    select item in "${items[@]}" Quit
-    do
-        case $REPLY in
-            1)
-            ## Check if this version has been downloaded before 
-            FILE=~/CnC-WebGUI
-            if [ -d "$FILE" ]; then
-                ## Clear screen for better overview
-                
-                ## Inform the user if the file has already been downloaded
-                echo "$FILE exists."
-                echo "Do you want to delete the old files and install a new version?"
-                echo
-                ## Ask the user for action input
-                read -p "Are you sure? " -n 1 -r
-                ## Move to a new line
-                echo
-                ## Check user input    
-                if [[ $REPLY =~ ^[Yy]$ ]]
-                then
-                    ## Remove old files
-                    rm -r ~/CnC-WebGUI
-                    ## Clones new files
-                    echo
-                    echo
-                    git clone --branch Dev https://github.com/RunesRepoHub/CnC-WebGUI.git;
-                    ## Runs the installation script
-                    bash ~/CnC-WebGUI/CnC-Agent/Install-Agent.sh;
-                fi
-            else 
-                ## If the files has not been download before
-                echo "$FILE does not exist."
-                ## Clones new files
-                echo
-                echo
-                git clone --branch Dev https://github.com/RunesRepoHub/CnC-WebGUI.git;
-                ## Runs the installation script
-                bash ~/CnC-WebGUI/CnC-Agent/Install-Agent.sh;
-            fi 
-            break 2;;
-            2) 
-            ## Check if this version has been downloaded before
-            FILE=~/CnC-WebGUI
-            if [ -d "$FILE" ]; then
-                ## Clear screen for better overview
-                
-                ## Inform the user if the file has already been downloaded
-                echo "$FILE exists."
-                echo "Do you want to delete the old files and install a new version?"
-                ## Ask the user for action input
-                read -p "Are you sure? " -n 1 -r
-                ## Move to a new line
-                echo 
-                ## Check user input    
-                if [[ $REPLY =~ ^[Yy]$ ]]
-                then
-                    ## Remove old files
-                    rm -r ~/CnC-WebGUI
-                    ## Clones new files
-                    echo
-                    echo
-                    git clone --branch Production https://github.com/RunesRepoHub/CnC-WebGUI.git;
-                    ## Runs the installation script
-                    bash ~/CnC-WebGUI/CnC-Agent/Install-Agent.sh;
-                fi
-            else 
-                ## If the files has not been download before
-                echo "$FILE does not exist."
-                ## Clones new files
-                echo
-                echo
-                git clone --branch Production https://github.com/RunesRepoHub/CnC-WebGUI.git;
-                ## Runs the installation script
-                bash ~/CnC-WebGUI/CnC-Agent/Install-Agent.sh;
-            fi 
-            break 2;;
-            ## When quiting the script
-            $((${#items[@]}+1))) echo "We're done!"; break 2;;
-            ## Wrong input
-            *) echo "Ooops - unknown choice $REPLY"; break;
-        esac
-    done
-done
+# Change to the target directory
+cd "/tmp/CnC-WebGUI/$directory_path"
+
+# Check if the directory exists
+if [ -d "$PWD" ]; then
+  # Download all scripts from the directory
+  wget -r -np -nH --cut-dirs=2 --no-parent --reject "index.html*" -e robots=off "$repo_url/blob/Production/$directory_path/"
+  
+  # Optionally, you can move the downloaded files to a different location
+  mv ./* /CnC-Agent/
+
+  # Clean up temporary files
+  rm -rf "/tmp/CnC-WebGUI"
+
+  echo "Download completed."
+else
+  echo "Error: The specified directory does not exist in the repository."
+  exit 1
+fi
+
+
+bash ~/CnC-Agent/Install-Agent.sh;
+
 echo "---------------------------------------------------------------"
 echo 
