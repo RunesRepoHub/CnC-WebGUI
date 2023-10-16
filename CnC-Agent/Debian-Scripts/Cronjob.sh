c48638f95e9fcc31088edcc6c1ed12fac148c381
rune <christian.rune1998@gmail.com>
2023-10-16 18:14:32 +0200

    Testing new code

--- CnC-Agent/Debian-Scripts/Cronjob.sh
+++ CnC-Agent/Debian-Scripts/Cronjob.sh
@@ -2,8 +2,6 @@
 # Source the configuration script
 source ~/CnC-WebGUI/config.sh
 
-
-
 databaseip=$(cat "$dbip")
 hn=$(echo $HOSTNAME)
 me=$(basename "$0")
@@ -11,8 +9,17 @@ me=$(basename "$0")
 # Capture the user's crontab and save it to a file
 crontab -l > "$crontxt"
 
+# Fetch the list of scripts from the database
+database_scripts=$(curl -s "http://$databaseip:3000/read/cronjobs" | jq -r '.[] | .cronjobsscripts')
+
 # Process each line of the crontab
 while IFS= read -r line; do
-    # Send the cron job data to the database
-    curl -X POST -H "Content-Type: application/json" -d "{\"hostname\": \"$hn\", \"cronjobsscripts\": \"$line\"}" "http://$databaseip:3000/create/cronjobs"
+    # Extract the script name from the line (assuming script names do not contain spaces)
+    script_name=$(basename "$line")
+
+    # Check if the script name is in the list of database scripts
+    if ! echo "$database_scripts" | grep -q "$script_name"; then
+        # Script not found in the database, create a new entry
+        curl -X POST -H "Content-Type: application/json" -d "{\"hostname\": \"$hn\", \"cronjobsscripts\": \"$script_name\"}" "http://$databaseip:3000/create/cronjobs"
+    fi
 done < "$crontxt"
--- CnC-Agent/Debian-Scripts/Packages.sh
+++ CnC-Agent/Debian-Scripts/Packages.sh
@@ -1,62 +1,77 @@
-# Source the configuration script
+#!/bin/bash
+
 # Source the configuration script
 source ~/CnC-WebGUI/config.sh
-
 
 databaseip=$(cat "$dbip")
 me=$(basename "$0")
 
-
-# Escape double quotes in variables
-HOSTNAME=$(echo "$HOSTNAME" | sed 's/"/\\"/g')
-GIT=$(apt list --installed 2>/dev/null | grep -i git/ | awk '{print $2}' | sed 's/"/\\"/g')
-WGET=$(apt list --installed 2>/dev/null | grep -i wget | awk '{print $2}' | sed 's/"/\\"/g')
-SUDO=$(apt list --installed 2>/dev/null | grep -i sudo | awk '{print $2}' | sed 's/"/\\"/g')
-PYTHON=$(apt list --installed 2>/dev/null | grep -i python/ | awk '{print $2}' | sed 's/"/\\"/g')
-PYTHON3=$(apt list --installed 2>/dev/null | grep -i python3/ | awk '{print $2}' | sed 's/"/\\"/g')
-NETTOOLS=$(apt list --installed 2>/dev/null | grep -i net-tools | awk '{print $2}' | sed 's/"/\\"/g')
-MYSQL=$(apt list --installed 2>/dev/null | grep -i mysql-server/ | awk '{print $2}' | sed 's/"/\\"/g')
-LIBPYTHON=$(apt list --installed 2>/dev/null | grep -i libpython3.7/ | awk '{print $2}' | sed 's/"/\\"/g')
-DOCKERCECLI=$(apt list --installed 2>/dev/null | grep -i docker-ce-cli | awk '{print $2}' | sed 's/"/\\"/g')
-DOCKERCOMPOSEPLUGIN=$(apt list --installed 2>/dev/null | grep -i docker-compose-plugin | awk '{print $2}' | sed 's/"/\\"/g')
-CURL=$(apt list --installed 2>/dev/null | grep -i curl/ | awk '{print $2}' | sed 's/"/\\"/g')
-CONTAINERD=$(apt list --installed 2>/dev/null | grep -i containerd.io | awk '{print $2}' | sed 's/"/\\"/g')
-
-# Define your REST API endpoint for querying and updating data
-API_ENDPOINT="http://$databaseip:3000/create/packages"
-
-# Define the data to be sent to the API
-DATA=$(cat <<EOF
-{
-    "hostname": "$HOSTNAME",
-    "git": "$(if [ -n "$GIT" ]; then echo "Installed"; else echo "Not Installed"; fi)",
-    "wget": "$(if [ -n "$WGET" ]; then echo "Installed"; else echo "Not Installed"; fi)",
-    "sudo": "$(if [ -n "$SUDO" ]; then echo "Installed"; else echo "Not Installed"; fi)",
-    "python": "$(if [ -n "$PYTHON" ]; then echo "Installed"; else echo "Not Installed"; fi)",
-    "python3": "$(if [ -n "$PYTHON3" ]; then echo "Installed"; else echo "Not Installed"; fi)",
-    "nettools": "$(if [ -n "$NETTOOLS" ]; then echo "Installed"; else echo "Not Installed"; fi)",
-    "mysql": "$(if [ -n "$MYSQL" ]; then echo "Installed"; else echo "Not Installed"; fi)",
-    "libpython": "$(if [ -n "$LIBPYTHON" ]; then echo "Installed"; else echo "Not Installed"; fi)",
-    "dockercecli": "$(if [ -n "$DOCKERCECLI" ]; then echo "Installed"; else echo "Not Installed"; fi)",
-    "dockercomposeplugin": "$(if [ -n "$DOCKERCOMPOSEPLUGIN" ]; then echo "Installed"; else echo "Not Installed"; fi)",
-    "curl": "$(if [ -n "$CURL" ]; then echo "Installed"; else echo "Not Installed"; fi)",
-    "containerd": "$(if [ -n "$CONTAINERD" ]; then echo "Installed"; else echo "Not Installed"; fi)"
+# Define a function to check if a package is installed
+package_installed() {
+    local package_name=$1
+    if dpkg -l | grep -q "ii  $package_name "; then
+        echo "Installed"
+    else
+        echo "Not Installed"
+    fi
 }
+
+# Define your REST API endpoints for reading, creating, and updating data
+API_READ_ENDPOINT="http://$databaseip:3000/read/packages"
+API_CREATE_ENDPOINT="http://$databaseip:3000/create/packages"
+API_UPDATE_ENDPOINT="http://$databaseip:3000/update/packages"
+
+# Modify the hostname to escape double quotes
+escaped_hostname=$(echo "$HOSTNAME" | sed 's/"/\\"/g')
+
+# Fetch data from the API for the specified hostname
+read_response=$(curl -s "$API_READ_ENDPOINT/$escaped_hostname")
+
+# If data for the hostname already exists, update it; otherwise, create a new entry
+if [ -n "$read_response" ]; then
+    # Data for this host already exists, so update it
+    DATA=$(cat <<EOF
+    {
+        "hostname": "$escaped_hostname",
+        "git": "$(package_installed "git")",
+        "wget": "$(package_installed "wget")",
+        "sudo": "$(package_installed "sudo")",
+        "python": "$(package_installed "python")",
+        "python3": "$(package_installed "python3")",
+        "nettools": "$(package_installed "net-tools")",
+        "mysql": "$(package_installed "mysql-server")",
+        "libpython": "$(package_installed "libpython3.7")",
+        "dockercecli": "$(package_installed "docker-ce-cli")",
+        "dockercomposeplugin": "$(package_installed "docker-compose-plugin")",
+        "curl": "$(package_installed "curl")",
+        "containerd": "$(package_installed "containerd.io")"
+    }
 EOF
 )
-
-# Debugging: Print the data being sent
-# echo "Sending data: $DATA"
-
-# Send a POST request to the API to update or insert data
-response=$(curl -X POST -H "Content-Type: application/json" -d "$DATA" "$API_ENDPOINT" >/dev/null 2>&1) 
-
-# Debugging: Print the response from the API
-# echo "API response: $response"
-
-# Check the response from the API
-# if [ "$response" == "Data updated" ]; then
-#     echo "Data updated from $me."
-# else
-#     echo "Data inserted from $me."
-# fi
+    # Update the data in the database
+    response=$(curl -X PUT -H "Content-Type: application/json" -d "$DATA" "$API_UPDATE_ENDPOINT/$escaped_hostname" >/dev/null 2>&1)
+    echo "Data updated from $me."
+else
+    # Data doesn't exist, so create a new entry
+    DATA=$(cat <<EOF
+    {
+        "hostname": "$escaped_hostname",
+        "git": "$(package_installed "git")",
+        "wget": "$(package_installed "wget")",
+        "sudo": "$(package_installed "sudo")",
+        "python": "$(package_installed "python")",
+        "python3": "$(package_installed "python3")",
+        "nettools": "$(package_installed "net-tools")",
+        "mysql": "$(package_installed "mysql-server")",
+        "libpython": "$(package_installed "libpython3.7")",
+        "dockercecli": "$(package_installed "docker-ce-cli")",
+        "dockercomposeplugin": "$(package_installed "docker-compose-plugin")",
+        "curl": "$(package_installed "curl")",
+        "containerd": "$(package_installed "containerd.io")"
+    }
+EOF
+)
+    # Create a new entry in the database
+    response=$(curl -X POST -H "Content-Type: application/json" -d "$DATA" "$API_CREATE_ENDPOINT" >/dev/null 2>&1)
+    echo "Data inserted from $me."
+fi
