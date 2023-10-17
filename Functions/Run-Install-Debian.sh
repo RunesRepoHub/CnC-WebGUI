a931f909e5558dd9443bce82a9424adc2446a421
rune <christian.rune1998@gmail.com>
2023-10-17 14:54:39 +0200

    Hotfix

--- Functions/Run-Install-Debian.sh
+++ Functions/Run-Install-Debian.sh
@@ -41,7 +41,7 @@ echo
 
 PS3="Select the what version you want to install:"
 
-items=("${Red}Development${NC} "${Green}Production${NC}")
+items=("${Red}Development${NC}" "${Green}Production${NC}")
 
 ## Select Version of code to download
 
