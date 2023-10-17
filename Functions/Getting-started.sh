9f69ee33372d588441199f94b73469e26cb79df9
rune <christian.rune1998@gmail.com>
2023-10-17 17:14:51 +0200

    Complete design upgrade

--- Functions/Getting-started.sh
+++ Functions/Getting-started.sh
@@ -20,8 +20,8 @@ while true; do
             ## Clear screen for better overview
             
             ## Inform the user if the file has already been downloaded
-            echo "$FILE exists."
-            echo "Do you want to delete the old files and install a new version?"
+            echo -e "${Red}$FILE exists.${NC}"
+            echo -e "${Red}Do you want to delete the old files and install a new version?${NC}"
             echo
             ## Ask the user for action input
             read -p "Are you sure? " -n 1 -r
@@ -32,8 +32,6 @@ while true; do
             ## Remove old files
             rm -rf ~/CnC-Agent
             ## Clones new files
-            echo
-            echo
             git clone --branch Dev https://github.com/RunesRepoHub/CnC-Agent.git;
             ## Runs the installation script
             else
@@ -41,21 +39,15 @@ while true; do
             fi
             else 
             ## If the files has not been download before
-            echo "$FILE does not exist."
+            echo -e "${Green}$FILE does not exist.${NC}"
             ## Clones new files
-            echo
-            echo
             git clone --branch Dev https://github.com/RunesRepoHub/CnC-Agent.git;
             ## Runs the installation script
             fi; 
             wget -O ~/CnC-Agent/config.sh "$get_config_url" > /dev/null 2>&1;
             bash "$Install_Agent"; break 2;;  
             3) bash "$Uninstall"; break 2;;  
-            $((${#items[@]}+1))) echo "We're done!"; break 2;;
-            *) echo "Oops - unknown choice $REPLY"; break;
+            $((${#items[@]}+1))) echo; echo -e "${Green}Go to https://runesrepohub.github.io/CnC-WebGUI/ for more information"${NC}; break;
         esac
     done
 done
-
-echo 
-echo
