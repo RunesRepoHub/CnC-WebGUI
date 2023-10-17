a36b2f35d7ce1f050788b8a0e4fa767675338cb5
rune <christian.rune1998@gmail.com>
2023-10-17 18:37:19 +0200

    Minor fix Uninstall #13

--- Functions/Uninstall.sh
+++ Functions/Uninstall.sh
@@ -9,42 +9,25 @@ docker compose -f "$compose" -p cnc-webgui down
 docker network rm cnc-webgui_cncnetwork
 
 # Remove the docker images used by the dockers 
-docker rm cnc-web:$version
-docker rm cnc-pg:$version
-docker rm cnc-node-api:$version
+docker rmi cnc-web:$version
+docker rmi cnc-pg:$version
+docker rmi cnc-node-api:$version
 
 # Remove docker volume used by the dockers
 docker volume rm cnc-webgui_dbdata
 
-# Define the paths you want to search for in cron jobs
-target_paths=("$pack_cron" "$over_cron" "$cron_cron")
+# Function to remove a cron job
+remove_cron_job() {
+    local script_name="$1"
+    crontab -l | grep -v "$script_name" | crontab -
+}
 
-# Get the current user's crontab
-current_user=$(whoami)
-crontab -l > /tmp/user_crontab
+# Remove the specified cron jobs
+remove_cron_job "$pack_cron"
+remove_cron_job "$over_cron"
+remove_cron_job "$cron_cron"
 
-# Filter and remove cron jobs with the specified paths
-filtered_crontab=""
-while IFS= read -r line; do
-    should_remove=0
-    for path in "${target_paths[@]}"; do
-        if [[ "$line" == *"$path"* ]]; then
-            should_remove=1
-            break
-        fi
-    done
-    if [ "$should_remove" -eq 0 ]; then
-        filtered_crontab+="$line"$'\n'
-    fi
-done < /tmp/user_crontab
-
-# Install the updated crontab
-echo "$filtered_crontab" | crontab -
-
-# Clean up temporary files
-rm /tmp/user_crontab
-
-echo "Cron jobs with specified paths removed from your crontab."
+echo "Cron jobs removed from your crontab."
 
 # Remove both repos
 rm -rf ~/CnC-WebGUI
