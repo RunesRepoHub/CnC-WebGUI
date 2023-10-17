cecc75804cdcb045f3a8813ef53f549b059af772
rune <christian.rune1998@gmail.com>
2023-10-17 18:59:09 +0200

    Fuly ready Uninstall #13

--- Functions/Uninstall.sh
+++ Functions/Uninstall.sh
@@ -15,6 +15,19 @@ docker volume rm cnc-webgui_dbdata
 
 sudo sed -i '/Packages\.sh\|Cronjob\.sh\|Overview\.sh/d' /etc/crontab
 
+# Define the paths of the scripts you want to remove from the crontab
+pack_cron="/root/CnC-Agent/Debian-Scripts/Packages.sh"
+over_cron="/root/CnC-Agent/Debian-Scripts/Overview.sh"
+cron_cron="/root/CnC-Agent/Debian-Scripts/Cronjob.sh"
+
+# Get the current user's crontab
+current_user=$(whoami)
+
+# Display the current crontab and remove the specified jobs
+crontab -l | grep -v "$pack_cron" | grep -v "$over_cron" | grep -v "$cron_cron" | crontab -
+
+echo "Cron jobs removed from your crontab."
+
 # Remove both repos
 rm -rf ~/CnC-WebGUI
 rm -rf ~/CnC-Agent
