d53dce81d8a0a507e2042dc2aeb45e9c557a18ee
rune <christian.rune1998@gmail.com>
2023-10-17 16:28:19 +0200

    Updated

--- Functions/Run-Install-Debian.sh
+++ Functions/Run-Install-Debian.sh
@@ -51,7 +51,7 @@ while true; do
                 ## Clear screen for better overview
                 
                 ## Inform the user if the file has already been downloaded
-                echo "${Red}$FILE exists.${NC}"
+                echo -e "${Red}$FILE exists.${NC}"
                 echo -e "${Red}Do you want to delete the old files and install a new version?${NC}"
                 echo
                 ## Ask the user for action input
@@ -64,8 +64,6 @@ while true; do
                     ## Remove old files
                     rm -r ~/CnC-WebGUI
                     ## Clones new files
-                    echo
-                    echo
                     git clone --branch Dev https://github.com/RunesRepoHub/CnC-WebGUI.git;
                     ## Runs the installation script
                     bash ~/CnC-WebGUI/Functions/Getting-started.sh;
@@ -74,10 +72,8 @@ while true; do
                 fi
             else 
                 ## If the files has not been download before
-                echo "${Green}$FILE does not exist.${NC}"
+                echo -e "${Green}$FILE does not exist.${NC}"
                 ## Clones new files
-                echo
-                echo
                 git clone --branch Dev https://github.com/RunesRepoHub/CnC-WebGUI.git;
                 ## Runs the installation script
                 bash ~/CnC-WebGUI/Functions/Getting-started.sh;
@@ -90,20 +86,16 @@ while true; do
                 ## Clear screen for better overview
                 
                 ## Inform the user if the file has already been downloaded
-                echo "${Red}$FILE exists.${NC}"
-                echo "${Red}Do you want to delete the old files and install a new version?${NC}"
+                echo -e "${Red}$FILE exists.${NC}"
+                echo -e "${Red}Do you want to delete the old files and install a new version?${NC}"
                 ## Ask the user for action input
                 read -p "Are you sure? " -n 1 -r
-                ## Move to a new line
-                echo 
                 ## Check user input    
                 if [[ $REPLY =~ ^[Yy]$ ]]
                 then
                     ## Remove old files
                     rm -r ~/CnC-WebGUI
                     ## Clones new files
-                    echo
-                    echo
                     git clone --branch Production https://github.com/RunesRepoHub/CnC-WebGUI.git;
                     ## Runs the installation script
                     bash ~/CnC-WebGUI/Functions/Getting-started.sh;
@@ -112,7 +104,7 @@ while true; do
                 fi
             else 
                 ## If the files has not been download before
-                echo "${Green}$FILE does not exist.${NC}"
+                echo -e "${Green}$FILE does not exist.${NC}"
                 ## Clones new files
                 echo
                 echo
@@ -131,16 +123,15 @@ done
 
 
 
-serverinstallcon="cat ~/CnC-WebGUI/.serverinstallcon"
-clientinstallcon="cat ~/CnC-Agent/.clientinstallcon"
+serverinstallcon=$(cat ~/CnC-WebGUI/.serverinstallcon)
+clientinstallcon=$(cat ~/CnC-Agent/.clientinstallcon)
 
 if [ -f "$serverinstallcon" ] && [ -f "$clientinstallcon" ]; then
-
-if [[ "$clientinstallcon" == "yes" && "$serverinstallcon" == "yes" ]]; then
+    if [ "$clientinstallcon" == "yes" ] && [ "$serverinstallcon" == "yes" ]; then
         echo -e "${Green}Installation has been successful${NC}"
     else 
-        echo -e "${Green}Installation has failed${NC}"
+        echo -e "${Red}Installation has failed${NC}"
     fi
 else 
-    echo -e "${Green}Installation has aborted${NC}"
+    echo -e "${Red}Installation has aborted${NC}"
 fi
