e11a9b6d30c070b55cf1206b2fb76351b60ef4c3
rune <christian.rune1998@gmail.com>
2023-10-19 15:20:47 +0200

    Testing added searchable pages

--- CnC-WebGUI/Nginx-Docker/Php/overview.php
+++ CnC-WebGUI/Nginx-Docker/Php/overview.php
@@ -1,40 +1,39 @@
-<?php
-// API endpoint URL
-$apiUrl = "http://cnc-api:3000/read/info";
-
-// Fetch data from the API
-$data = file_get_contents($apiUrl);
-
-// Check if the request was successful
-if ($data === false) {
-    die("Failed to fetch data from the API.");
-}
-
-// Parse the JSON data
-$cronjobs = json_decode($data, true);
-
-// Check if the JSON was successfully parsed
-if ($cronjobs === null) {
-    die("Failed to parse JSON data.");
-}
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <link rel="stylesheet" href="../style.css">
+    <script>
+        function searchTable() {
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById('searchInput');
+            filter = input.value.toUpperCase();
+            table = document.getElementById('cronjobTable');
+            tr = table.getElementsByTagName('tr');
+
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName('td')[0]; // Change the index to match the column you want to search
+                if (td) {
+                    txtValue = td.textContent || td.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = '';
+                    } else {
+                        tr[i].style.display = 'none';
+                    }
+                }
+            }
+        }
+    </script>
 </head>
 <body>
   <div class="overskift">
-  <h1>Cronjobs</h1>
-  <a href="../Php/packages.php">Packages</a>
-  <a href="../Php/cronjob.php">Cronjobs</a>
-  <a href="../Php/overview.php">Overview</a>
+    <h1>Cronjobs</h1>
+    <a href="../Php/packages.php">Packages</a>
+    <a href="../Php/cronjob.php">Cronjobs</a>
+    <a href="../Php/overview.php">Overview</a>
   </div>
-    <table>
+  <input type="text" id="searchInput" onkeyup="searchTable()" placeholder="Search for Hostname...">
+    <table id="cronjobTable">
         <tr>
-            <th>ID</th>
             <th>Hostname</th>
             <th>IP Address</th>
             <th>MAC Address</th>
@@ -43,7 +42,6 @@ if ($cronjobs === null) {
         </tr>
         <?php foreach ($cronjobs as $cronjob): ?>
             <tr>
-                <td><?php echo $cronjob['id']; ?></td>
                 <td><?php echo $cronjob['hostname']; ?></td>
                 <td><?php echo $cronjob['ipaddress']; ?></td>
                 <td><?php echo $cronjob['macaddress']; ?></td>
--- CnC-WebGUI/Nginx-Docker/Php/packages.php
+++ CnC-WebGUI/Nginx-Docker/Php/packages.php
@@ -1,38 +1,40 @@
-<?php
-// API endpoint URL
-$apiUrl = "http://cnc-api:3000/read/packages";
-
-// Fetch data from the API
-$data = file_get_contents($apiUrl);
-
-// Check if the request was successful
-if ($data === false) {
-    die("Failed to fetch data from the API.");
-}
-
-// Parse the JSON data
-$packagesData = json_decode($data, true);
-
-// Check if the JSON was successfully parsed
-if ($packagesData === null) {
-    die("Failed to parse JSON data.");
-}
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <link rel="stylesheet" href="../style.css">
+    <script>
+        // Function to filter the table rows based on user input
+        function searchTable() {
+            var input, filter, table, tr, td, i, j, txtValue;
+            input = document.getElementById("searchInput");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("packageTable");
+            tr = table.getElementsByTagName("tr");
+
+            for (i = 0; i < tr.length; i++) {
+                tr[i].style.display = "none"; // Hide all rows by default
+                td = tr[i].getElementsByTagName("td");
+                for (j = 0; j < td.length; j++) {
+                    txtValue = td[j].textContent || td[j].innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = ""; // Display the row if a match is found
+                        break; // No need to check other columns in the same row
+                    }
+                }
+            }
+        }
+    </script>
 </head>
 <body>
-  <div class="overskift">
-  <h1>Packages</h1>
-  <a href="../Php/packages.php">Packages</a>
-  <a href="../Php/cronjob.php">Cronjobs</a>
-  <a href="../Php/overview.php">Overview</a>
-  </div>
-    <table>
+    <div class="overskift">
+        <h1>Packages</h1>
+        <a href="../Php/packages.php">Packages</a>
+        <a href="../Php/cronjob.php">Cronjobs</a>
+        <a href="../Php/overview.php">Overview</a>
+    </div>
+    <input type="text" id="searchInput" onkeyup="searchTable()" placeholder="Search for packages...">
+
+    <table id="packageTable">
         <tr>
             <th>Hostname</th>
             <th>Package Name</th>
@@ -44,6 +46,7 @@ if ($packagesData === null) {
             echo "<td>" . $row['hostname'] . "</td>";
             echo "<td>" . $row['packagename'] . "</td>";
             echo "<td>" . $row['installed'] . "</td>";
+            echo "</tr>";
         }
         ?>
     </table>
